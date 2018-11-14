d04396e3886a666abddbdc8a3182de6d69bf0a4f
Sherif Eid <sherif.hamdy.1995@gmail.com>
2018-11-14 17:08:00 +0200

    added dynamodb table

--- src/main/java/com/aws/codestar/projecttemplates/handler/Calculator.java
+++ src/main/java/com/aws/codestar/projecttemplates/handler/Calculator.java
@@ -1,9 +1,21 @@
 package com.aws.codestar.projecttemplates.handler;
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTableMapper;
+
 public class Calculator {
+    private DynamoDBTableMapper<User, String, ?> userMapper;
 
 
+    public Calculator() {
+        DynamoDBMapper mapper = new DynamoDBMapper(AmazonDynamoDBClientBuilder.defaultClient());
+        userMapper = mapper.newTableMapper(User.class);
+    }
     public int add(Pair pair) {
+        User user = new User();
+        user.setName("sherif");
+        userMapper.save(user);
         return pair.getX() + pair.getY();
     }
 }
---
+++ src/main/java/com/aws/codestar/projecttemplates/handler/User.java
@@ -0,0 +1,29 @@
+package com.aws.codestar.projecttemplates.handler;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+
+@DynamoDBTable(tableName = "UserTable")
+public class User {
+
+    @DynamoDBHashKey
+    private String id;
+
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
